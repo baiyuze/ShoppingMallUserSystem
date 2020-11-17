c961f6cee0574b8b974120e8ebe1c455c97efae7
baiyuze <1347417308@qq.com>
2020-11-18 01:17:24 +0800

    添加注释

--- src/main/java/com/ruan/usersystem/config/AuthenticationInterceptor.java
+++ src/main/java/com/ruan/usersystem/config/AuthenticationInterceptor.java
@@ -69,13 +69,13 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     //controller执行之后，且页面渲染之前调用
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("---------postHandle--------");
+//        System.out.println("---------postHandle--------");
     }
 
     //页面渲染之后调用，一般用于资源清理操作
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("---------afterCompletion--------");
+//        System.out.println("---------afterCompletion--------");
     }
 
 }
